eb84729b438c1f484faced618a381f1284b40ad7
Valeria <valerija.kozlova@tktk.ee>
2024-05-13 11:12:58 +0300

    merging two projects

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res)=>{
     let url = 'https://api.themoviedb.org/3/movie/218?api_key=80932c92af95c93e664e5e8d17873a77';
@@ -32,6 +33,55 @@ app.get('/', (req, res)=>{
             poster: posterUrl
         });    
     });
+    
+});
+
+app.get('/search', (req, res) => {
+    res.render('search', {movieDetails:''});
+});
+
+app.post('/search', (req, res) => {
+    let userMovieTitle = req.body.movieTitle;
+
+    let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=80932c92af95c93e664e5e8d17873a77&query=${userMovieTitle}`;
+    let genresUrl = 'https://api.themoviedb.org/3/genre/movie/list?api_key=80932c92af95c93e664e5e8d17873a77&language=en-US';
+   
+    let endpoints = [movieUrl, genresUrl];
+
+    axios.all(endpoints.map((endpoint) => axios.get(endpoint)))
+    .then(axios.spread((movie, genres) => {
+        const [movieRaw] = movie.data.results;
+        const movieGenreIds = movieRaw.genre_ids;        
+        const movieGenres = genres.data.genres; 
+            
+        let movieGenresArray = [];
+
+        for(let i = 0; i < movieGenreIds.length; i++) {
+            for(let j = 0; j < movieGenres.length; j++) {
+                if(movieGenreIds[i] === movieGenres[j].id) {
+                    movieGenresArray.push(movieGenres[j].name)
+                }
+            }
+        }
+
+        let genresToDisplay = '';
+        movieGenresArray.forEach(genre => {
+            genresToDisplay = genresToDisplay+ `${genre}, `;
+        });
+
+        genresToDisplay = genresToDisplay.slice(0, -2) + '.';
+            
+        let movieData = {
+            title: movieRaw.title,
+            year: new Date(movieRaw.release_date).getFullYear(),
+            genres: genresToDisplay,
+            overview: movieRaw.overview,
+            posterUrl: `https://image.tmdb.org/t/p/w500/${movieRaw.poster_path}`,
+        };
+    
+        res.render('search', {movieDetails: movieData});
+        
+    }));
     
 });
 
